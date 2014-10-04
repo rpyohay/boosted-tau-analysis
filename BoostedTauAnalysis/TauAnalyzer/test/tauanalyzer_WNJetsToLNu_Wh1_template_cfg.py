ea2442d116dee3cdb5abe70343bcb6353c2f4572
Rachel <Rachel.Yohay@cern.ch>
2014-10-04 18:29:13 +0200

    reverted MET collection to one ending in PFlow for backwards compatibility

--- BoostedTauAnalysis/TauAnalyzer/test/tauanalyzer_WNJetsToLNu_Wh1_template_cfg.py
+++ BoostedTauAnalysis/TauAnalyzer/test/tauanalyzer_WNJetsToLNu_Wh1_template_cfg.py
@@ -399,7 +399,7 @@ process.highMTMuHadIsoTauAnalyzer = cms.EDAnalyzer(
     'HIGHMTISOTAUANALYZEROUTFILE'
     ),
     tauTag = cms.InputTag('IsoBVetoFilter'),
-    METTag = cms.InputTag('patType1CorrectedPFMetNotSmeared'),
+    METTag = cms.InputTag('patType1CorrectedPFMetPFlow'),
     muonTag = cms.InputTag('WIsoMuonSelector'),
     muonPFIsoPUSubtractionCoeff = cms.double(0.5),
     genMatchedMuonTag = cms.InputTag('WIsoMuonSelector'),
@@ -493,11 +493,11 @@ process.output = cms.OutputModule(
 
 #MET filter
 process.METFilter.minMET = cms.double(30.)
-process.METFilter.METTag = cms.InputTag("patType1CorrectedPFMetNotSmeared")
+process.METFilter.METTag = cms.InputTag("patType1CorrectedPFMetPFlow")
 
 #MT filter
 process.MTFilter.minMT = cms.double(50.)
-process.MTFilter.METTag = cms.InputTag("patType1CorrectedPFMetNotSmeared")
+process.MTFilter.METTag = cms.InputTag("patType1CorrectedPFMetPFlow")
 process.highMTFilter = process.MTFilter.clone()
 process.lowMTFilter = process.MTFilter.clone()
 process.lowMTFilter.passFilter = cms.bool(False)
