82733a351ba9731ffbb43e1ce7cebc225915ce0c
Rachel <Rachel.Yohay@cern.ch>
2015-06-23 14:56:51 +0200

    added checking of signal jobs executed locally

--- BoostedTauAnalysis/TauAnalyzer/test/checkForJobFailures.sh
+++ BoostedTauAnalysis/TauAnalyzer/test/checkForJobFailures.sh
@@ -17,24 +17,31 @@ fi
 echo "Checking for STL exceptions and CMS exceptions..."
 echo "-------------------------------------------------"
 grep -r xception $version/LSFJOB_*
+grep -r xception $version/*.txt
 echo
 
 echo "Checking for segfaults..."
 echo "-------------------------------------------------"
 grep -r egmentation $version/LSFJOB_*
 grep -r iolation $version/LSFJOB_*
+grep -r egmentation $version/*.txt
+grep -r iolation $version/*.txt
 echo
 
 echo "Checking for file copy failures..."
 echo "-------------------------------------------------"
 grep -r "Error accessing" $version/LSFJOB_*
 grep -r quota $version/LSFJOB_*
+grep -r "Error accessing" $version/*.txt
+grep -r quota $version/*.txt
 echo
 
 echo "Checking for job timeouts..."
 echo "-------------------------------------------------"
 grep -r Cputime $version/LSFJOB_*
 grep -r Killed $version/LSFJOB_*
+grep -r Cputime $version/*.txt
+grep -r Killed $version/*.txt
 echo
 
 exit 0
