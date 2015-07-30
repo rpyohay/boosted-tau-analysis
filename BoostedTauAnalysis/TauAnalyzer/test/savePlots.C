c93faa498546d21eca3825d71d805f2b86fd1d77
Rachel <Rachel.Yohay@cern.ch>
2015-07-30 16:48:56 +0200

    commented out references to FranTestDir

--- BoostedTauAnalysis/TauAnalyzer/test/savePlots.C
+++ BoostedTauAnalysis/TauAnalyzer/test/savePlots.C
@@ -41,10 +41,10 @@ void savePlots(const string& outputVersion, const string& a1Mass, const string& 
   //for error bands
   gStyle->SetErrorX(0.5);
 
-  const string FranTestDir("/myNMSSMAnalysis/CMSSW_5_3_11/src/BoostedTauAnalysis/TauAnalyzer/test/plotsToSave");
+//   const string FranTestDir("/myNMSSMAnalysis/CMSSW_5_3_11/src/BoostedTauAnalysis/TauAnalyzer/test/plotsToSave/");
   //save directory
-  const string saveDir("/afs/cern.ch/user/" + userDir + "/" + user + FranTestDir +
-		       /*"/AN-13-254/notes/AN-13-254/trunk/figures/"*/"/");
+  const string saveDir("/afs/cern.ch/user/" + userDir + "/" + user + 
+		       "/AN-13-254/notes/AN-13-254/trunk/figures/"/*FranTestDir*/);
 
   // //HLT path
   // string HLTPath("_IsoMu24_eta2p1");
